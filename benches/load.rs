84e4198f597e48faf9bfcdacc54aa1fe2876b340
Mark Nokalt <marknokalt@live.com>
2020-11-11 14:43:30 -0300

    chore: Add more benchmarks

--- benches/load.rs
+++ benches/load.rs
@@ -2,11 +2,29 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::fs::File;
 
 const TORRENT_PATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/data/torrent.benc");
+const DEEP1_PATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/data/max_depth.benc");
+const DEEP2_PATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/data/really_deep.benc");
 
 pub fn load(c: &mut Criterion) {
-    let mut file = File::open(TORRENT_PATH).unwrap();
-
     c.bench_function("load torrent file", |b| {
+        let mut file = File::open(TORRENT_PATH).unwrap();
+
+        b.iter(|| {
+            bencode::load(black_box(&mut file)).expect("Failed loading file");
+        })
+    });
+
+    c.bench_function("load max depth", |b| {
+        let mut file = File::open(DEEP1_PATH).unwrap();
+
+        b.iter(|| {
+            bencode::load(black_box(&mut file)).expect("Failed loading file");
+        })
+    });
+
+    c.bench_function("load really deep", |b| {
+        let mut file = File::open(DEEP2_PATH).unwrap();
+
         b.iter(|| {
             bencode::load(black_box(&mut file)).expect("Failed loading file");
         })
